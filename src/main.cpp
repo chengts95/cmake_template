98d1744e812d50923b74fa0eb60bc30c2bd30446
chengts95 <chengts95@163.com>
2020-10-12 19:40:43 -0600

    first circuit

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,33 @@
 #include <RLC.h>
 #include <matplotlib.hpp>
 namespace plt = matplotlibcpp;
-void swap(int & a, int & b)
+void swap(int &a, int &b)
 {
     int temp;
     temp = b;
     b = a;
     a = temp;
 }
-
+const int M = 2;
+#define IDX(i, j, lda) (i * lda + j)
+Real G[M * M] = {0};
+Real b[M] = {0};
 int main(int argc, char const *argv[])
 {
     RLCBranch rlc;
     std::vector<Real> data;
-    for(int i=0;i<int(1e6);i++){
-        data.emplace_back(sin(377.*i*1e-6));
+    Real R1 = 5, R2 = 10;
+    Real I2 = 1;
+    Real U1 = 1.0, Rs = 1e-6;
+    Real I1 = U1 / Rs;
 
-    }
-    plt::plot(data);
-    plt::show();
+    G[IDX(0, 0, M)] += 1.0 / R1;
+    G[IDX(1, 2, M)] -= 1.0 / R1;
+    G[IDX(2, 1, M)] -= 1.0 / R1;
+    G[IDX(0, 0, M)] += 1.0 / Rs;
+    G[IDX(1, 1, M)] += 1.0 / R2;
+    G[IDX(1, 1, M)] += 1.0 / R1;
+    b[0]+=I1;
+    b[1]+=I2;
     return 0;
 }
